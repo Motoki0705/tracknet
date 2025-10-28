f4a01edf73aa361c38312db7f408af883b8a9032
Motoki0705 <kamimura098@gmail.com>
2025-10-28 16:26:28 +0900

    pref: add convnext's outputs structure

--- demo/convnext_demo.py
+++ demo/convnext_demo.py
@@ -6,7 +6,6 @@ url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = load_image(url)
 print("Loaded image size:", image.size) # (640, 480)
 
-# ConvNeXtの4ステージを全部取り出す（stage1〜stage4）
 pretrained_model_name = "facebook/dinov3-convnext-base-pretrain-lvd1689m"
 processor = AutoImageProcessor.from_pretrained(pretrained_model_name)
 model = AutoModel.from_pretrained(
@@ -26,6 +25,15 @@ try:
     print(type(outputs.hidden_states), len(outputs.hidden_states))  # tuple, 個数はモデルによる
     for i, h in enumerate(outputs.hidden_states):
         print(f" hidden_states[{i}] shape: {h.shape}")
+    '''
+    result:
+    <class 'tuple'> 5
+    hidden_states[0] shape: torch.Size([1, 3, 224, 224])
+    hidden_states[1] shape: torch.Size([1, 128, 56, 56])
+    hidden_states[2] shape: torch.Size([1, 256, 28, 28])
+    hidden_states[3] shape: torch.Size([1, 512, 14, 14])
+    hidden_states[4] shape: torch.Size([1, 1024, 7, 7])
+    '''
 except AttributeError:
     print("No hidden_states in outputs")
     print("Available keys:", outputs.keys())
